6ecc76fd89a04b5e53ae59e14b4abb9efb352a22
Adamstbright <113826509+Adamstbright@users.noreply.github.com>
2023-03-29 20:41:18 +0100

    Add lines to pages

--- main.py
+++ main.py
@@ -11,9 +11,15 @@ for index, row in df.iterrows():
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
     pdf.line(10, 21, 200, 21)
 
+    # Add the lines
+    for line_height in range(21, 290,  10):
+        pdf.line(10, line_height, 200, line_height)
+
+
+
     # Set the footer
     pdf.ln(255)
-    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_font(family="Times", style="I", size=12)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="R")
 
@@ -21,9 +27,13 @@ for index, row in df.iterrows():
     for i in range(row["Pages"] - 1):
         pdf.add_page()
 
+        # Add the lines
+        for line_height in range(21, 280, 10):
+            pdf.line(10, line_height, 200, line_height)
+
         # Set the footer
         pdf.ln(265)
-        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_font(family="Times", style="I", size=12)
         pdf.set_text_color(100, 100, 100)
         pdf.cell(w=0, h=12, txt=row["Topic"], align="R")
 
